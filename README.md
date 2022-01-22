a3a0f32fe949f6900d39382cfad2234b92bdcf49
Ben Hunter <code@benhunter.me>
2022-01-22 17:13:33 -0600

    Add README and clean up Advice.java

---
+++ README.md
@@ -0,0 +1,11 @@
+# @ControllerAdvice
+
+[@ControllerAdvice Docs](https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/bind/annotation/ControllerAdvice.html)
+
+Specialization of @Component for classes that declare @ExceptionHandler, @InitBinder, or @ModelAttribute methods to be shared across multiple @Controller classes.
+
+# @RestControllerAdvice
+
+[@RestControllerAdvice Docs](https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/bind/annotation/RestControllerAdvice.html)
+
+A convenience annotation that is itself annotated with @ControllerAdvice and @ResponseBody.
--- src/main/java/me/benhunter/springcontrolleradviceexample/Advice.java
+++ src/main/java/me/benhunter/springcontrolleradviceexample/Advice.java
@@ -1,6 +1,5 @@
 package me.benhunter.springcontrolleradviceexample;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,7 +9,9 @@ public class Advice {
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<String> handleBadRequest(BadRequestException badRequestException) {
-//        return ResponseEntity.badRequest().body("Bad bad bad");
-        return new ResponseEntity<>("Bad bad bad", HttpStatus.BAD_REQUEST);
+        return ResponseEntity.badRequest().body("Bad bad bad");
+
+        // Alternate way to create a ResponseEntity:
+        // return new ResponseEntity<>("Bad bad bad", HttpStatus.BAD_REQUEST);
     }
 }
